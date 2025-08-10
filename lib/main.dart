e7652c1a66c9b624a2c935cfd8fd422bbb3106b4
shahnewazIbrahim <shahnewaz886@gmail.com>
2025-08-10 10:55:39 +0600

    dynamic data show

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return ChangeNotifierProvider(
       create: (_) => AuthService(),
       child: Consumer<AuthService>(
